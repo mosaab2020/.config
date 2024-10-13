7a577b2d9d1efaf05c48eb4a4f4b957f7e7b3145
mosaab2020 <mosaabomar@gmail.com>
2024-10-13 23:08:23 +0400

    update config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,16 +43,17 @@ return {
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
+    enabled = false,
     config = function()
       require("telescope").load_extension("ui-select")
-      -- require("telescope").setup {
-      --   extensions = {
-      --     ["ui-select"] = {
-      --       require("telescope.themes").get_dropdown {
-      --       }
-      --     }
-      --   }
-      -- }
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
     end
   },
   {
